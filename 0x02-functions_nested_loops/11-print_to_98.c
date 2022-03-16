3a037e5c01f1e0d625634dcc2c047a18e4ec187e
Cozyamy <cozyamy@gmail.com>
2022-03-16 02:44:56 +0000

    Prints all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,11 +8,19 @@
 
 void print_to_98(int n);
 {
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n < 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
 
 	return (0);
 }
