f23c77afe9f4744659fb45501624323c13bc9261
Cozyamy <cozyamy@gmail.com>
2022-03-30 02:38:41 -0700

    Working with recursion in C

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line@s: pointer to a string
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: pointer to a string
  */
 
 void _puts_recursion(char *s)
