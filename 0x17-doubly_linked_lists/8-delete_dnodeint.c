163eaa2d4d869d318ce40766cd047fb32c9b9960
Cozyamy <cozyamy@gmail.com>
2022-06-09 20:37:09 +0100

    Doubly linked lists in C

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,49 +1,63 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at index index of a
- * dlistint_t list.
- * @head: Pointer to head.
- * @index: Index of node to delete.
- * Return: 1 if it succeeded, -1 if it failed.
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
+ * delete_dnodeint_at_index - deltes a node in a doubly linked list
+ * at a given index
+ * @head: double pointer to the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *delete_node = *head;
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
 
-	if (!head || !(*head))
+	if (*head == NULL || dlistint_len(temp) < index + 1)
 		return (-1);
 
-	/*Only 1 node*/
-
-	/*Find node to delete*/
-	for (; index > 1; index--)
-	{
-		delete_node = delete_node->next;
-		if (!delete_node)
-			return (-1);
-	}
-
-	/*If there is the first node*/
 	if (!index)
 	{
-		/*If there is only a node*/
-		if (!(delete_node->next))
-			*head = NULL;
-		else
-			*head = delete_node->next;
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
+		return (1);
 	}
-	else
-		delete_node = delete_node->next;
 
-	if (delete_node->prev)
-		delete_node->prev->next = delete_node->next;
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-	if (delete_node->next)
-		delete_node->next->prev = delete_node->prev;
-
-	free(delete_node);
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
 
 	return (1);
 }
